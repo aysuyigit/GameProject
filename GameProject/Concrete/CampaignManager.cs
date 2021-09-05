cf94307fe6eafe82b4d74cb7f7c9d56c39f7d698
aysuyigit <aysuyigit00mail.com>
2021-09-05 13:29:25 +0300

    Add project files

---
+++ GameProject/Concrete/CampaignManager.cs
@@ -0,0 +1,26 @@
+﻿using GameProject.Abstract;
+using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Concrete
+{
+    public class CampaignManager : ICampaignService
+    {
+        public void Add(Campaign campaign)
+        {
+            throw new NotImplementedException();
+        }
+      
+        public void Delete(Campaign campaign)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Campaign campaign)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameProject/Concrete/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using GameProject.Abstract;
+using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProje.Consrete
+{
+    class GameManager : IGameService
+    {
+        public void Add(Game game)
+        {
+            Console.WriteLine(game.GameName + "  : Oyunu Eklendi. ");
+        }
+
+        public void Delete(Game game)
+        {
+            Console.WriteLine(game.GameName + "  : Oyunu Silidi. ");
+        }
+
+
+        public void Update(Game game)
+        {
+            Console.WriteLine(game.GameName + "  :Oyunu Güncellendi. ");
+        }
+
+        
+    }
+}
